eba362c96ccb34cf7f27b364738a1f8979c86dbe
Lina Djihane <lina.djihane@gmail.com>
2023-11-17 19:23:55 +0100

    phase1 modified not working

--- Recharge.java
+++ Recharge.java
@@ -1,7 +1,10 @@
 package up.mi.ald.root;
 
 import java.time.temporal.ValueRange;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Iterator;
 
 // Recharge is a utility class
 public class Recharge {
@@ -11,7 +14,7 @@ public class Recharge {
 		int indexOfCity = -1;
 		System.out.println("Please enter the index of the city where the charging post needs to be installed");
 		agg.showCities();
-		indexOfCity = checkCityIndex(agg, indexOfCity);
+		indexOfCity = getCityIndex(agg, indexOfCity);
 		if(indexOfCity == -1) return;
 		if(!agg.rechargeZones.add(indexOfCity)) {
 			// will only be added in the set if it doesn't exist already
@@ -24,24 +27,45 @@ public class Recharge {
 		int indexOfCity = -1;
 		System.out.println("Please enter the index of the city you want to remove the charging point from.");
 		agg.showCities();
-		indexOfCity = checkCityIndex(agg, indexOfCity);
+		indexOfCity = getCityIndex(agg, indexOfCity); 
 		if(indexOfCity == -1) return;
-		boolean canBeRemoved = true;
-		for(int i = 0; i < agg.getNumberOfCities(); i++) {
-			if(checkAccessibility(agg, indexOfCity, i) == false) {
-				canBeRemoved = false;
-			}
-		}
-		if (canBeRemoved) {
-			agg.rechargeZones.remove(indexOfCity);
-			System.out.println("charging point removed from city C" + indexOfCity);
+		agg.rechargeZones.remove(indexOfCity);
+		if(checkAccessibility(agg) == false) {
+			System.out.println("Charging point can't be removed from this city.");
+			agg.rechargeZones.add(indexOfCity);
 		} else {
-			System.out.println("Charging point can't be removed from this city.1");
+			System.out.println("Charging point removed from city C" + indexOfCity);
 		}
 		agg.showRechargeZones();
 	}
 	
-	private static int checkCityIndex(Agglomeration agg, int index) {
+	private static boolean checkAccessibility(Agglomeration agg) {
+		Set<Integer> coveredZones = new HashSet<>(); // Set of cities that have access to a charging point directly or indirectly
+		coveredZones.addAll(agg.rechargeZones);
+		Iterator<Integer> cz = coveredZones.iterator();
+		while (cz.hasNext()) {
+			Integer zoneIndex = cz.next();
+			for(int i = 0; i < agg.getNumberOfCities(); i++) {
+				if(agg.routes[zoneIndex][i] == true) {
+					coveredZones.add(i);
+				}
+			}
+			cz = coveredZones.iterator();
+		}
+		Set<Integer> cityIndexes = new HashSet<>();
+		for(int i = 0; i < agg.getNumberOfCities(); i++) {
+			cityIndexes.add(i);
+		}
+		Set<Integer> nonCoveredZones = new HashSet<>(cityIndexes);
+		nonCoveredZones.removeAll(coveredZones);
+		if(!nonCoveredZones.isEmpty()) {
+			System.out.println("The following cities don't have access to a charging zone");
+			System.out.println(nonCoveredZones);
+		}
+		return coveredZones.equals(cityIndexes);
+	}
+	
+	private static int getCityIndex(Agglomeration agg, int index) {
 		try {
 			index = Integer.parseInt(scanner.nextLine());
 			if(ValueRange.of(0, agg.getNumberOfCities()-1).isValidIntValue(index)) {
@@ -49,27 +73,11 @@ public class Recharge {
 			}
 			else {
 				System.out.println("*****ERROR*****\nInvalid city index.\nIt doesn't correspond to an index of a city that exists in the list of cities.\n***************");
-				return -1; // if index is incorrect we return -1
+				return -1; // if index is invalid we return -1
 			}
 		} catch (Exception e) {
 			System.out.println("*****ERROR*****\nIndex of a city must be a positive integer\n***************");
 		}
 		return index;
 	}
-	
-	private static boolean checkAccessibility(Agglomeration agg, int indexOfCity, int i) {
-		// if a city x has a charging point 
-		if(agg.rechargeZones.contains(indexOfCity)) {
-			// we check whether the cities that are connected to city x have a charging point
-			if(agg.routes[indexOfCity][i] == true && agg.rechargeZones.contains(i)) {
-				// if they do have a charging point we can remove the charging point of city x
-				return true;
-			} else if(agg.routes[indexOfCity][i] == true && !agg.rechargeZones.contains(i)){
-				System.out.println("C" + i + " won't have access to a charging point.");
-				return false;
-			}
-		}
-		return true;
-	}
-
 }
