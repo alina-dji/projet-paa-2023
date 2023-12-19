feb746b5cfe1d8539ce98ebeb50a82d698460aba
Lina Djihane <lina.djihane@gmail.com>
2023-12-19 17:24:40 +0100

    updated CLI.java

--- CLI.java
+++ CLI.java
@@ -5,28 +5,44 @@ import java.io.IOException;
 // command line interface: all the messages destined to communicate with the user go here
 public class CLI {
 	
-	public static void getfilePath() {
+	public static String getfilePath() {
 		System.out.println("Input the path to the file containning your agglomeration data:");
 		String path = Scanner.scanFilePath();
 		while (path.isEmpty()) {
 			System.out.println("Your file path is invalid. try again:");
 			path = Scanner.scanFilePath();
 		}
+		return path;
 	}
 	
-	public static void createAgglomeration(String path) {
+	public static Agglomeration createAgglomeration(String path) {
+		Agglomeration agg = null;
 		try {
-			Agglomeration agg = AgglomerationFileParser.parseFile(path);
-		} catch (IllegalDataFormattingException | CityNotFoundException | IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			agg = AgglomerationFileParser.parseFile(path);
+		} catch (IOException ioe) {
+			System.out.println("An error occured when trying to access your data file. Please check that the location you specified is correct and that there are no other issues preventing the reading operation.");
+			endProgram();
+		} catch (CityNotFoundException cnfe) {
+			String message = cnfe.toString();
+			System.out.println(message.substring(message.indexOf(":") + 2));
+			endProgram();
+		} catch (IllegalDataFormattingException idfe) {
+			String message = idfe.toString();
+			System.out.println(message.substring(message.indexOf(":") + 2));
+			endProgram();
 		}
+		return agg;
 	}
+	
 	public static void mainMenu() {
 		
 	}
 	
 	public static void manualSolutionMenu() {
 		
+	}
+	
+	public static void endProgram() {
+		System.out.println("Program ended");
 	}
 }
--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ package up.mi.ald.root;
 
 public class Test {
 	public static void main(String[] args) {
-		
+		String path = CLI.getfilePath();
+		CLI.createAgglomeration(path);
 		/*Agglomeration agg;
 		try {
 			agg = AgglomerationFileParser.parseFile("C:\\Users\\dell\\eclipse-workspace\\projet-paa-2023\\src\\up\\mi\\ald\\root\\aggdata.ca");
